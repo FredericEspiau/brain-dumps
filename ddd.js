e4451a363ffe62f9b82b004b2f814e5e6bc875f0
Frederic Espiau <7319147+FredericEspiau@users.noreply.github.com>
2020-10-16 15:38:16 +0200

    Update ddd.js

--- ddd.js
+++ ddd.js
@@ -1,3 +1,4 @@
 # Domain Driven Design
 
 - [If you're new to DDD and not sure where to start, this process will guide you step-by-step](https://github.com/ddd-crew/ddd-starter-modelling-process)
+- [Some design approaches to enforcing a business rule requiring no duplicates](https://github.com/ardalis/DDD-NoDuplicates)
